79fcd459e3424534a0b6e387696128ec3d3a82f5
Killian KOPP <killiankopp@gmail.com>
2024-01-05 02:18:23 +0100

    test css

--- src/components/Dechaineur.jsx
+++ src/components/Dechaineur.jsx
@@ -1,12 +1,15 @@
 import * as React from "react"
+import { dechaineur } from "./Dechaineur.module.css"
 
 const Dechaineur = ({name, title, description, url_profile}) => {
     return (
-        <div>
-            <h3>{name}</h3>
-            <img src={url_profile} alt="{name}" width={200} />
-            <strong>{title}</strong>
-            <p>{description}</p>
+        <div className={dechaineur}>
+            <img src={url_profile} alt="{name}" />
+            <div>
+                <h3>{name}</h3>
+                <strong>{title}</strong>
+                <p>{description}</p>
+            </div>
         </div>
     );
 };
